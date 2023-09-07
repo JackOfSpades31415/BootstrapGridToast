d1c9a557d9292720b56c0ad7bcbe878383adc4d5
JackOfMinisculeTrades <jack314159265358979323@gmail.com>
2023-09-07 18:53:54 -0500

    Fixed thanks to teacher's help

--- client.js
+++ client.js
@@ -4,35 +4,14 @@ $(function(){
     $('.code').on('click', function(e) {
         e.preventDefault();
         
-    
-        switch ($(".code").index(this)){
-            case 0 :
-                $('.mr-auto').data("Product", "Chef Anton's Italian Seasoning");
-                $('#code').data("CODE", "1234");
-                break;
-
-            case 1 :
-                $('.mr-auto').data("Product", "Genen Shouyu");
-                $('#code').data("CODE", "GR33N");
-                break;
-
-            case 2 :
-                $('.mr-auto').data("Product", "Schoggi Schokolade");
-                $('#code').data("CODE", "H4LF50");
-                break;
-                
-        }
+        $('#name').html($(this).data('name'));
+        $('#code').html($(this).data('code'));
         // first pause the audio (in case it is still playing)
         toast.pause();
         // reset the audio
         toast.currentTime = 0;
         // play audio
         toast.play();
-        var product = $( '.mr-auto' ).data( "Product" );
-        var code = $( '#code' ).data( "CODE" );
-
-        $( ".mr-auto" ).text( "" + product );
-        $( "#code" ).text( "" + code );
         $('#liveToast').toast({ autohide: false }).toast('show');
 
         
